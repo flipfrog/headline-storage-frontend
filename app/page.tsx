b269cdaa3c45317031e68508d6fef8f2294a9102
flipfrog <>
2024-09-01 15:50:19 +0900

    try fetch on the first time on this environment.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import FirstComponent from "@/app/_components/FirstComponent";
 
-export default function Home() {
+export default async function Home() {
+
+    const data = await fetch('http://localhost/api/headlines', { cache: 'no-store' });
+    // TODO: Define Type Of Headline.
+    const headlines = await data.json();
+    console.log('*** headlines='+JSON.stringify(headlines));
+
     return <FirstComponent firstName="Albert" />;
 }
