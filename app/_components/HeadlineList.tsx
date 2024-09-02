a1fd0bc1dab4476ff905717d5ec2a0ab91ed5a9b
flipfrog <24383461+flipfrog@users.noreply.github.com>
2024-09-02 17:21:59 +0900

    add headlines filter and tidy up code.

--- app/_components/HeadlineList.tsx
+++ app/_components/HeadlineList.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
@@ -12,81 +11,16 @@ import TableFooter from '@mui/material/TableFooter';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
-import FirstPageIcon from '@mui/icons-material/FirstPage';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import LastPageIcon from '@mui/icons-material/LastPage';
+import {FormControlLabel, Checkbox, Grid2} from "@mui/material";
 
 import { Headline } from "@/app/page";
+import TablePaginationActions from "@/app/_components/TablePaginationActions";
+import {useState} from "react";
 
-interface TablePaginationActionsProps {
-    count: number;
-    page: number;
-    rowsPerPage: number;
-    onPageChange: (
-        event: React.MouseEvent<HTMLButtonElement>,
-        newPage: number,
-    ) => void;
-}
-
-function TablePaginationActions(props: TablePaginationActionsProps) {
-    const theme = useTheme();
-    const { count, page, rowsPerPage, onPageChange } = props;
-
-    const handleFirstPageButtonClick = (
-        event: React.MouseEvent<HTMLButtonElement>,
-    ) => {
-        onPageChange(event, 0);
-    };
-
-    const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        onPageChange(event, page - 1);
-    };
-
-    const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        onPageChange(event, page + 1);
-    };
-
-    const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-    };
-
-    return (
-        <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-            <IconButton
-                onClick={handleFirstPageButtonClick}
-                disabled={page === 0}
-                aria-label="first page"
-            >
-                {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-            </IconButton>
-            <IconButton
-                onClick={handleBackButtonClick}
-                disabled={page === 0}
-                aria-label="previous page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            </IconButton>
-            <IconButton
-                onClick={handleNextButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="next page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-            </IconButton>
-            <IconButton
-                onClick={handleLastPageButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="last page"
-            >
-                {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-            </IconButton>
-        </Box>
-    );
-}
-
-const HeadlineList = ({ headlines }: { headlines: Headline[] }) => {
+const HeadlineList = ({headlines, categories }: {
+    headlines: Headline[],
+    categories: string[],
+}) => {
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
@@ -108,55 +42,84 @@ const HeadlineList = ({ headlines }: { headlines: Headline[] }) => {
         setPage(0);
     };
 
+    const [ checkedCategories, setCheckedCategories ] = useState(categories);
+    const [ filteredHeadlines, setFilteredHeadlines ] = useState(headlines);
+
+    const onChangeCategoryCheckbox = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
+        const subjectCategory = categories[index];
+        const checked = event.target.checked;
+        const newCheckedCategories = checked ? checkedCategories.concat(subjectCategory) : checkedCategories.filter(category => category !== subjectCategory);
+        const newFilteredHeadlines = headlines.filter(headline => newCheckedCategories.includes(headline.category));
+        setCheckedCategories(newCheckedCategories);
+        setFilteredHeadlines(newFilteredHeadlines);
+    };
+
     return (
         <>
-            <TableContainer component={Paper}>
-                <Table sx={{minWidth: 650}} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>ID</TableCell>
-                            <TableCell>Title</TableCell>
-                            <TableCell>forward ref IDs</TableCell>
-                            <TableCell>backward ref IDs</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {(rowsPerPage > 0
-                                ? headlines.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                : headlines
-                        ).map((headline : Headline) => (
-                            <TableRow key={headline.id}>
-                                <TableCell>{headline.id}</TableCell>
-                                <TableCell>{headline.title}</TableCell>
-                                <TableCell>{headline.forward_refs?.map(ref => `${ref.id}:${ref.title}`).join(', ')}</TableCell>
-                                <TableCell>{headline.backward_refs?.map(ref => `${ref.id}:${ref.title}`).join(', ')}</TableCell>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid2 container spacing={2}>
+                    {
+                        categories.map((category, index) => (
+                            <Grid2 key={index} size={3}>
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox defaultChecked onChange={(event: React.ChangeEvent<HTMLInputElement>) => onChangeCategoryCheckbox(event, index)}/>
+                                    }
+                                    label={category}/>
+                            </Grid2>)
+                        )
+                    }
+                </Grid2>
+                <TableContainer component={Paper}>
+                    <Table sx={{minWidth: 650}} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>ID</TableCell>
+                                <TableCell>Title</TableCell>
+                                <TableCell>Category</TableCell>
+                                <TableCell>forward ref IDs</TableCell>
+                                <TableCell>backward ref IDs</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                    <TableFooter>
-                        <TableRow>
-                            <TablePagination
-                                rowsPerPageOptions={[5, 10, 25, {label: 'All', value: -1}]}
-                                colSpan={4}
-                                count={headlines.length}
-                                rowsPerPage={rowsPerPage}
-                                page={page}
-                                slotProps={{
-                                    select: {
-                                        inputProps: {
-                                            'aria-label': 'rows per page',
+                        </TableHead>
+                        <TableBody>
+                            {(rowsPerPage > 0
+                                    ? filteredHeadlines.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                    : filteredHeadlines
+                            ).map((headline : Headline) => (
+                                <TableRow key={headline.id}>
+                                    <TableCell>{headline.id}</TableCell>
+                                    <TableCell>{headline.title}</TableCell>
+                                    <TableCell>{headline.category}</TableCell>
+                                    <TableCell>{headline.forward_refs?.map(ref => `${ref.id}:${ref.title}`).join(', ')}</TableCell>
+                                    <TableCell>{headline.backward_refs?.map(ref => `${ref.id}:${ref.title}`).join(', ')}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                        <TableFooter>
+                            <TableRow>
+                                <TablePagination
+                                    rowsPerPageOptions={[5, 10, 25, {label: 'All', value: -1}]}
+                                    colSpan={4}
+                                    count={headlines.length}
+                                    rowsPerPage={rowsPerPage}
+                                    page={page}
+                                    slotProps={{
+                                        select: {
+                                            inputProps: {
+                                                'aria-label': 'rows per page',
+                                            },
+                                            native: true,
                                         },
-                                        native: true,
-                                    },
-                                }}
-                                onPageChange={handleChangePage}
-                                onRowsPerPageChange={handleChangeRowsPerPage}
-                                ActionsComponent={TablePaginationActions}
-                            />
-                        </TableRow>
-                    </TableFooter>
-                </Table>
-            </TableContainer>
+                                    }}
+                                    onPageChange={handleChangePage}
+                                    onRowsPerPageChange={handleChangeRowsPerPage}
+                                    ActionsComponent={TablePaginationActions}
+                                />
+                            </TableRow>
+                        </TableFooter>
+                    </Table>
+                </TableContainer>
+            </Box>
         </>
     );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,38 @@ import HeadlineList from "@/app/_components/HeadlineList";
 
 export type Headline = {
   id: number;
+  category: string;
   title: string;
   description: string | null;
   forward_refs?: Headline[];
   backward_refs?: Headline[];
 };
 
-export default async function Home() {
+const getCategories = async () : Promise<string[]> => {
+  type Received = {
+    categories: string[];
+  };
+  const data = await fetch('http://localhost/api/categories', { cache: 'no-store' });
+  const json : Received = await data.json();
+  return json.categories;
+};
+
+const getHeadlines = async () : Promise<Headline[]> => {
   type Received = {
     headlines: Headline[];
   };
   const data = await fetch('http://localhost/api/headlines', { cache: 'no-store' });
-  const received : Received = await data.json();
-  const headlines: Headline[] = received.headlines;
+  const json : Received = await data.json();
+  return json.headlines;
+};
+
+export default async function Home() {
+  const categories: string[] = await getCategories();
+  const headlines: Headline[] = await getHeadlines();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <HeadlineList headlines={headlines} />
+      <HeadlineList categories={categories} headlines={headlines} />
     </main>
   );
 }
